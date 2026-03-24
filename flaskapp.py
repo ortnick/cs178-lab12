9296b9bc5088b7a695362c6ff4b13014af21ada0
nick ortman <ortnick@gmail.com>
2026-03-24 11:15:40 -0500

    Testing deploy pipeline

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-# Lab 12 - nick ortman
+# Lab 12 - Nick Ortman
 # Flask needs to know the name of this file to find templates and static files
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # render_template loads templates/home.html and sends it to the browser
-    return render_template('home.html', page_title="My Flask Site"
+    return render_template('home.html', page_title="My Flask Site")
 
 
 # ============================================================
